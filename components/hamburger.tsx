6a866c60cfeec3b9f24c737fbe2566bbe0c5565f
soloplxya <69782911+soloplxya@users.noreply.github.com>
2022-10-08 15:29:48 +0800

    feat: add the pages

--- components/hamburger.tsx
+++ components/hamburger.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
+import Link from 'next/link';
+
 import { slide as Menu } from 'react-burger-menu';
 
 export default (props:any) => {
   return (
     <Menu>
-      <a className="menu-item" href="/vote">
-        Vote
-      </a>
-      <a className="menu-item" href="/propose">
-        Propose
-      </a>
-      <a className="menu-item" href="/experience">
-        Experience
-      </a>
-      <a className="menu-item" href="/community">
-        Community
-      </a>
-      <a className="menu-item" href="/mission">
-        Mission
-      </a>
+      <Links />
     </Menu>
   );
 };
+
+export const Links = () => (<>
+  <div><Link href="/"><a className='font-bold p-4'>Home</a></Link></div>
+  <div><Link href="/vote"><a className='font-bold p-4'>Vote</a></Link></div>
+  <div><Link href="/propose"><a className='font-bold p-4'>Propose</a></Link></div>
+  <div><Link href="/experience"><a className='font-bold p-4'>Experience</a></Link></div>
+  <div><Link href="/community"><a className='font-bold p-4'>Community</a></Link></div> 
+  <div><Link href="/mission"><a className='font-bold p-4'>Mission</a></Link></div>
+</>)
---
+++ pages/mission.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+import Hamburger from '../components/hamburger'
+
+const Mission: NextPage = () => {
+  return (
+    <main> 
+      <div id='outer-container'>
+        <Hamburger pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+      </div> 
+      <div id="page-wrap" className={styles.container} >
+          <h1>This will be the mission page.</h1>
+          <h2></h2>
+      </div>
+    </main>
+  )
+}
+
+export default Mission
