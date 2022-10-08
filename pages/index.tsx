e39316d857ed4ecadc1696d3175d3f6541a7a81e
soloplxya <shainesnake@gmail.com>
2022-10-09 01:49:58 +0800

    feat: add home page & mission page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import Hamburger from '../components/hamburger'
+import Typewriter from 'typewriter-effect';
+import { Parallax } from 'react-parallax';
 
 const Home: NextPage = () => {
   return (
@@ -10,9 +9,35 @@ const Home: NextPage = () => {
       <div id='outer-container'>
         <Hamburger pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
       </div> 
-      <div id="page-wrap" className={styles.container} >
-          <h1>This will be the homepage.</h1>
-          <h2></h2>
+      <div id="page-wrap"> 
+        <div style={{ textAlign: 'center' }}>
+        <Parallax bgImage='/farm.jpg' strength={600} style={{ width: '100vw' }}>
+          <div style={{ height: 600, width: '100vw' }} className="grid h-screen place-items-center">
+            <div className="text-4xl p-2">
+              <Typewriter
+                options={{
+                  autoStart: true, 
+                  loop: true,
+                }}
+                onInit={(typewriter: any) => {
+                  typewriter.typeString('<p style="color: #808080"> Welcome to <strong> Vegeverse </strong>!</p>')
+                    .pauseFor(300)
+                    .typeString('<p style="color: #000"> A solution that utilizes blockchain technology to help local farmers grow sustainably. </p>')
+                    .callFunction(() => {
+                      console.log('String typed out!');
+                    })
+                    .pauseFor(2500)
+                    .deleteAll()
+                    .callFunction(() => {
+                      console.log('All strings were deleted');
+                    })
+                    .start();
+                }}
+              />
+            </div> 
+          </div>
+        </Parallax>
+      </div>
       </div>
     </main>
   )
--- pages/mission.tsx
+++ pages/mission.tsx
@@ -5,32 +5,28 @@ import { Parallax } from 'react-parallax';
 
 const image1 =
   "https://images.unsplash.com/photo-1498092651296-641e88c3b057?auto=format&fit=crop&w=1778&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D";
-const image2 =
-  "https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5297440765001_5280261645001-vs.jpg?pubId=5104226627001&videoId=5280261645001";
 
-const inlineStyle = {
-    background: '#fff',
-    left: '50%',
-    top: '50%',
-    position: 'absolute',
-    padding: '20px',
-    transform: 'translate(-50%, -50%)',
-}
-  
+
 const Mission: NextPage = () => {
   return (
     <main> 
       <div id='outer-container'>
         <Hamburger pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
       </div> 
-      <div id="page-wrap" className={styles.container}  > 
+      <div id="page-wrap"> 
         <div style={{ textAlign: 'center' }}>
-        <Parallax bgImage={ image1 } strength={500}>
-          <div style={{ height: 1000, width: 1000 }} className="grid h-screen place-items-center">
+        <Parallax bgImage={ image1 } strength={700} style={{ width: '100vw' }}>
+          <div style={{ height: 400, width: '100vw' }} className="grid h-screen place-items-center">
             <div className="text-3xl p-2">OUR MISSION</div>
           </div>
         </Parallax>
-      < h1>| | |</h1>
+        <Parallax strength={700} style={{ width: '100vw' }}>
+          <div style={{ height: 500, width: '100vw' }} className="grid h-screen place-items-center">
+            <div className="text-3xl px-2 mx-10">
+              <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+          </div> 
+        </Parallax>
       </div>
       </div>
     </main>
