77c2c5e3e572744d59d392cc4bfb6ab8aa92cd79
soloplxya <69782911+soloplxya@users.noreply.github.com>
2022-10-08 17:10:26 +0800

    feat: intialize connection with metamask account

--- pages/vote.tsx
+++ pages/vote.tsx
@@ -1,6 +1,22 @@
 import type { NextPage } from 'next'
+import { ethers } from 'ethers'
 import styles from '../styles/Home.module.css'
 import Hamburger from '../components/hamburger'
+import { Button as MuiButton } from "@mui/material";
+
+async function connectToMetamask(){
+  // checks if metamask extension exists on chrome browser 
+  if (typeof window.ethereum !== 'undefined') {
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    console.log("Account:", await signer.getAddress());
+  // else we send an alert to the user
+  } else {
+    alert('MetaMask not loaded.')
+    return
+  }
+}
 
 const Vote: NextPage = () => {
   return (
@@ -11,6 +27,7 @@ const Vote: NextPage = () => {
       <div id="page-wrap" className={styles.container} >
           <h1>This will be the vote page.</h1>
           <h2></h2>
+          <MuiButton onClick={() => { connectToMetamask() }}> Connect </MuiButton> 
       </div>
     </main>
   )
