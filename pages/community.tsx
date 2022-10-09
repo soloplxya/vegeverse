e2c58044c23d83481014cbcc08265cbda16c0d9c
elsa <elsayeahx3@gmail.com>
2022-10-09 17:28:55 +0800

    feat: community page

--- pages/community.tsx
+++ pages/community.tsx
@@ -1,12 +1,143 @@
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
+
+import React  from "react";
+import { Badge, Box } from "@chakra-ui/react";
+import Image from "next/image";
+import { StarIcon } from "@chakra-ui/icons";
 
 const Community: NextPage = () => {
+
   return (
     <main>
-      <div id="page-wrap" className={styles.container}>
-        <h1>This will be the community page.</h1>
-        <h2></h2>
+      <div className="page-wrap grid grid-cols-5 mx-20">
+        <div className="col-start-2 col-span-3">
+        <p className="font-bold py-2 text-xl">See whats happening in Vegeverse community:</p>
+
+          <Box borderWidth="1px" borderRadius="lg" overflow="hidden" className="m-2">
+
+            <Box p="6">
+              <Box display="flex" alignItems="baseline">
+                <Badge borderRadius="full" px="2" colorScheme="green">
+                  Thread
+                </Badge>
+                <Box
+                  color="gray.500"
+                  fontWeight="semibold"
+                  letterSpacing="wide"
+                  fontSize="xs"
+                  textTransform="uppercase"
+                  ml="2"
+                >
+                  Conservation &bull; Agrotech
+                </Box>
+              </Box>
+
+              <Box
+                mt="1"
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
+                noOfLines={1}
+              >
+                Wild Boars Spotted in Paddy Farm #3
+              </Box>
+
+              <Box>
+                How can we help the animals thrive in our community of farm land
+              </Box>
+
+              <Box display="flex" mt="2" alignItems="center">
+                <Box as="span"  color="gray.600" fontSize="sm">
+                34 comments
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+          <Box borderWidth="1px" borderRadius="lg" overflow="hidden" className="m-2">
+
+            <Box p="6">
+              <Box display="flex" alignItems="baseline">
+                <Badge borderRadius="full" px="2" colorScheme="green">
+                  Thread
+                </Badge>
+                <Box
+                  color="gray.500"
+                  fontWeight="semibold"
+                  letterSpacing="wide"
+                  fontSize="xs"
+                  textTransform="uppercase"
+                  ml="2"
+                >
+                  Conservation &bull; Food Quality
+                </Box>
+              </Box>
+
+              <Box
+                mt="1"
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
+                noOfLines={1}
+              >
+                Crops have been attacked by pests
+              </Box>
+
+              <Box>
+                How can we improve our crop yield without using pesticide
+              </Box>
+
+              <Box display="flex" mt="2" alignItems="center">
+                <Box as="span"  color="gray.600" fontSize="sm">
+                12 comments
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+          <Box borderWidth="1px" borderRadius="lg" overflow="hidden" className="m-2">
+
+            <Box p="6">
+              <Box display="flex" alignItems="baseline">
+                <Badge borderRadius="full" px="2" colorScheme="blue">
+                  News
+                </Badge>
+                <Box
+                  color="gray.500"
+                  fontWeight="semibold"
+                  letterSpacing="wide"
+                  fontSize="xs"
+                  textTransform="uppercase"
+                  ml="2"
+                >
+                  Conservation &bull; Food Quality
+                </Box>
+              </Box>
+
+              <Box
+                mt="1"
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
+                noOfLines={1}
+              >
+                Great harvest from Paddy farm #14
+              </Box>
+
+              <Box>
+                Come see the photos
+              </Box>
+
+              <Box display="flex" mt="2" alignItems="center">
+                <Box as="span"  color="gray.600" fontSize="sm">
+                62 reactions
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+
+        </div>
+        <div>
+
+        </div>
       </div>
     </main>
   );
--- pages/vote.tsx
+++ pages/vote.tsx
@@ -38,8 +38,10 @@ export default function Vote() {
   async function execute() {
     if (typeof window.ethereum !== "undefined") {
       const tokenContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-      const governanceContractAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
-      const timelockContractAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+      const governanceContractAddress =
+        "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+      const timelockContractAddress =
+        "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 
       const tokenContract = new ethers.Contract(
         tokenContractAddress,
