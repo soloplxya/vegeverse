8b7aff06e3ece78ad68226ebd9effc2fbe56f107
elsa <elsayeahx3@gmail.com>
2022-10-09 12:08:43 +0800

    fix: padding for menu

--- components/hamburger.tsx
+++ components/hamburger.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import Link from 'next/link';
-
+import { MenuItem }from './menuitem'
 import { slide as Menu } from 'react-burger-menu';
 
-export default (props:any) => {
+
+export const Links = () => (<>
+  <MenuItem name="Home" href="/" /> 
+  <MenuItem name="Vote" href="/vote" />
+  <MenuItem name="Propose" href="/propose" /> 
+  <MenuItem name="Experience" href="/experience" /> 
+  <MenuItem name="Community" href="/community" /> 
+  <MenuItem name="Mission" href="/mission" /> 
+</>)
+
+
+export default () => {
   return (
     <Menu>
       <Links />
     </Menu>
   );
 };
-
-export const Links = () => (<>
-  <div><Link href="/"><a className='font-bold p-4'>Home</a></Link></div>
-  <div><Link href="/vote"><a className='font-bold p-4'>Vote</a></Link></div>
-  <div><Link href="/propose"><a className='font-bold p-4'>Propose</a></Link></div>
-  <div><Link href="/experience"><a className='font-bold p-4'>Experience</a></Link></div>
-  <div><Link href="/community"><a className='font-bold p-4'>Community</a></Link></div> 
-  <div><Link href="/mission"><a className='font-bold p-4'>Mission</a></Link></div>
-</>)
---
+++ components/menuitem.tsx
@@ -0,0 +1,7 @@
+import Link from 'next/link';
+
+export const MenuItem = (props: { name: string, href: string }) => (
+    <>
+        <div className='p-2'><Link href={props.href}><a className='font-bold'>{props.name}</a></Link></div>
+    </>
+);
